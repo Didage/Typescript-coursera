657f162fc88ebba644af78b077866e36086b9352
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2022-04-24 00:42:27 -0500

    MAteriales creados para la lección e clases

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+
+export const ap = new Aprendiz("Diego Alejandro", "Tovar García","./resources/avatar.png", 24, NivelEducativo.UNIVERSITARIO);
+
+console.log(ap);
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+export var ap = new Aprendiz("Diego Alejandro", "Tovar García", "./resources/avatar.png", 24, NivelEducativo.UNIVERSITARIO);
+console.log(ap);
